124de67e3a1112e0ee6ced8588a6c210e42e20aa
s3ponia <slava.shlyaga@gmail.com>
2020-04-20 20:56:08 +0300

    Fix logger. Fix util

--- src/include/logger.h
+++ src/include/logger.h
@@ -8,20 +8,29 @@
 
 class Logger {
 private:
-	static Logger *logger_instance;
-	static std::ofstream file;
+    std::ofstream file{LOG_FILE_PATH};
 
 public:
-	static Logger *get_instance();
-	void Debug(const std::string &);
-	void Info(const std::string &);
-	void Warning(const std::string &);
-	void Error(const std::string &);
+    static Logger *get_instance();
+
+    void Debug(const std::string &);
+
+    void Info(const std::string &);
+
+    void Warning(const std::string &);
+
+    void Error(const std::string &);
+
 private:
-	Logger() = default;
-	Logger(const Logger &);
-	void Write(const std::string &, const std::string &);
-	Logger &operator=(const Logger &) { return *this; };
+    ~Logger() = default;
+
+    Logger() = default;
+
+    Logger(const Logger &);
+
+    void Write(const std::string &, const std::string &);
+
+    Logger &operator=(const Logger &) { return *this; };
 };
 
 #endif //LOGGER_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "logger.h"
+#include <iostream>
+#include "logger.h"
 
 int main() {
-    Logger::get_instance()->Error("LOL");
-    Logger::get_instance()->Error("KEK");
-    Logger::get_instance()->Error("CHEBUREK");
+    Logger::get_instance()->Debug("KEK");
     return 0;
 }
